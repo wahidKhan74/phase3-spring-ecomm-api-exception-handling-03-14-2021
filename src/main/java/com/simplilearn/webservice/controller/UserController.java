95daff6fad7fd9ff4dffb8980b30b6b2a6e9d16e
Waheed Khan <wahid.khan74@gmail.com>
2021-03-14 15:36:21 +0000

    todo for user crud operations

---
+++ src/main/java/com/simplilearn/webservice/controller/UserController.java
@@ -0,0 +1,6 @@
+package com.simplilearn.webservice.controller;
+
+public class UserController {
+	// TODO :: WAP with Crud operations for user.
+	// TODO :: Add proper exception handling for the crud operations
+}
